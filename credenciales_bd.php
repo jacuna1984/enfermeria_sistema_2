ab66e7024495af028197605d76fa879e561fc799
Juan de la Cruz <juandelacruz84@gmail.com>
2021-11-08 17:35:34 -0300

    hoja_nueva_reg_est

--- credenciales_bd.php
+++ credenciales_bd.php
@@ -3,6 +3,6 @@ const DB_HOST = "localhost";
 const DB_PORT = 3306;
 const DB_NAME = "enfermeria";
 const DB_USER = "root";
-const DB_PASS = "";
+const DB_PASS = "root";
 ?>
 
--- registro_estudiante.php
+++ registro_estudiante.php
@@ -17,20 +17,24 @@ $fila = mysqli_fetch_array ($datos);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="css/styles.css"/>
-    <title>Inicio de Sesión - Enfermería</title>
+    <title>Inicio de Sesión - Estudiante</title>
     <script>
         function control() {
             var estado=true;
             if(document.getElementById("dni").value=="" || document.getElementById("password").value=="" || document.getElementById("nombre").value=="" || document.getElementById("apellido").value=="")
-        estado=false;}
+            estado=false;}
         return estado;
     </script>
 </head>
 <body id="body2">
     <header class="header">
         <h1 id="titulo">INSITUTO SUPERIOR DE FORMACIÓN DOCENTE "MERCEDES"</h1>
-                <img class="img-header" src="img/Logo-ISFD-Banderin-tricolor-01-500x500.png" width="130px" height="180px">
     </header>
+    
+    <img class="img-header" src="img/Logo-ISFD-Banderin-tricolor-01-500x500.png" width="130px" height="180px">
+
+    <h1 id="titulo">ESTUDIANTE</h1>
+    
     <div class="formusr">
         <form action="registro.php" method="post" onclick="return control()">
             <div class="form-group">
@@ -43,8 +47,9 @@ $fila = mysqli_fetch_array ($datos);
             </div>
             <div class="form-group">
                 <label for="password2">Ingrese nuevamente la contraseña</label>
-                <input required type="password" id="contraseña2" name="password2" size="60em" placeholder="Tipear nuevamente la contraseña">
+                <input required type="password" id="contraseña" name="password2" size="60em" placeholder="Tipear nuevamente la contraseña">
             </div>
+
             <hr>
             
             <div class="form-group">
@@ -83,33 +88,6 @@ $fila = mysqli_fetch_array ($datos);
             <select id="ciudad">
                 <option value="x1">Opcion X1</option>
             </select>
-            <div class="form-group2">
-                <label for="opc_usr">¿Es estudiante o Profesor?</label>
-                <select required name="tipo_usuario" class="form-control">
-                    <option value="#"></option>
-                    <option value="tipo_usuario">Estudiante</option>
-                    <option value="tipo_usuario">Profesor</option>
-                </select>
-            </div>
-            <div class="form-group2">
-                <label for="carrera">Seleccione su carrera</label>
-                <select required name="carrera" class="form-control">
-                    <option value="#"></option>
-                    <option value="enfermeria">Enfermería</option>
-                </select>
-            </div>
-            <div class="form-group2">
-                <label for="espacio_curricular">Espacio Curricular</label>
-                <select required name="espacio_curricular" class="form-control">
-                    <option value="#"></option>
-                    <option value="espacio_curricular">Practica I</option>
-                    <option value="espacio_curricular">Practica II</option>
-                    <option value="espacio_curricular">Practica III</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="ciclo_lectivo">Ciclo lectivo</label>
-                <input required type="text" id="ciclo_lectivo" name="ciclo_lectivo" size="60em" placeholder="Tipear su año lectivo">
             </div>
             <div class="btn-group btn-group-xl d-grid gap-2 col-6 mx-auto" role="group" aria-label="Basic example">
                 <button type="submit" class="btn btn-primary btn-lg" value="enviar">Enviar</button>
